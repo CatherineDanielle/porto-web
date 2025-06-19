0a8c11fd549117198f6c3604320c5815e14cdc05
cdanielle <96690358+CatherineDanielle@users.noreply.github.com>
2025-06-19 12:38:35 +0700

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,7 @@
 import React from 'react';
 import { BrowserRouter as Router, Routes, Route, Navigate, useLocation } from 'react-router-dom';
 import Home from './pages/Home';
-import Project from './pages/Project';
 
-// Debug component to show current route
 const RouteDebugger = () => {
   const location = useLocation();
   console.log('Current route:', location.pathname);
